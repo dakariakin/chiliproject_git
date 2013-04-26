52fecf92d37f533152d5244aba22eb532a764f74
Denis Karyakin <samhane.me@gmail.com>
2013-04-26 15:10:47 +0400

    Working creating new git user

--- app/controllers/git_main_controller.rb
+++ app/controllers/git_main_controller.rb
@@ -6,5 +6,24 @@ class GitMainController < ApplicationController
   end
 
   def index
+    if GitTable.exists?(User.current.id)
+      @user = GitTable.find(User.current.id)
+    else
+      redirect_to :action => 'new'
+    end
+  end
+
+  def new
+    @git_user = GitTable.new
+  end
+
+  def create
+    @git_user = GitTable.new(params[:git_user])
+    if @git_user.save
+      flash[:success] = l(:label_plugin_git_reg_success)
+      redirect_to :action => 'index'
+    else
+      render 'new'
+    end
   end
 end
