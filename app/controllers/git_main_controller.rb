ed3f6768540795338fd81f52ca600c17d824b224
Denis Karyakin <samhane.me@gmail.com>
2013-04-26 16:15:53 +0400

    Check if for current user already isset git account, check for unique login, for length of login (from 2 to 100)

--- app/controllers/git_main_controller.rb
+++ app/controllers/git_main_controller.rb
@@ -14,12 +14,18 @@ class GitMainController < ApplicationController
   end
 
   def new
-    @git_user = GitUser.new
+    if GitUser.exists?(User.current.id)
+      flash[:error] = l(:label_plugin_git_account_already_isset)
+      redirect_to :action => 'index'
+    else
+      @git_user = GitUser.new
+    end
   end
 
   def create
     @git_user = GitUser.new(params[:git_user])
     @git_user.blocked = 'F'
+    @git_user.id = User.current.id
     if @git_user.save
       flash[:success] = l(:label_plugin_git_reg_success)
       redirect_to :action => 'index'
--- app/models/git_user.rb
+++ app/models/git_user.rb
@@ -1,4 +1,6 @@
 class GitUser < ActiveRecord::Base
   unloadable
   belongs_to :user
+  validates_length_of :login, :in => 2..100, :message => l(:label_plugin_git_login_length_error)
+  validates_uniqueness_of :login, :message => l(:label_plugin_git_login_already_isset)
 end
