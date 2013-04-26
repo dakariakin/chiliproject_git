52743a21ee0b2393c25dc31a82e3f30a42c9d579
Denis Karyakin <samhane.me@gmail.com>
2013-04-26 12:50:36 +0400

    Rename labels in translate

--- init.rb
+++ init.rb
@@ -11,8 +11,8 @@ Redmine::Plugin.register :chiliproject_git do
   }, :partial => 'settings/index'
 
 #Menu
-  #menu :account_menu, :gittoi, {:controller => 'gittoi', :action => 'index'},
-  #     :caption => :label_plugin_gittoi_name, :if => Proc.new { User.current.logged? }, :before => :logout
-  #menu :admin_menu, :gittoi, {:controller => 'gittoi', :action => 'admin'},
-  #     :caption => :label_plugin_gittoi_name, :after => :users
+  #menu :account_menu, :chiliproject_git, {:controller => 'git_main', :action => 'index'},
+  #     :caption => :label_plugin_git_account, :if => Proc.new { User.current.logged? }, :before => :logout
+  #menu :admin_menu, :git, {:controller => 'git', :action => 'admin'},
+  #     :caption => :label_plugin_git_name, :after => :users
 end
