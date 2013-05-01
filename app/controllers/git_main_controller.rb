e20ffd75d4ba3eba0b271bf5074dc93aaf0f81a6
Denis Karyakin <samhane.me@gmail.com>
2013-05-02 00:49:44 +0400

    Check for empty key, for already isset key

--- app/controllers/git_main_controller.rb
+++ app/controllers/git_main_controller.rb
@@ -19,7 +19,7 @@ class GitMainController < ApplicationController
   def new
     if GitUser.exists?(User.current.id)
       flash[:error] = l(:label_plugin_git_account_already_isset)
-      redirect_to :action => 'index'
+      redirect_to :action => 'index', :flash => flash
     else
       @git_user = GitUser.new
     end
@@ -32,7 +32,7 @@ class GitMainController < ApplicationController
     if @git_user.save
       Dir.mkdir "#{Setting.plugin_chiliproject_git['dir_ssh_public']}/#{@git_user.login}"
       flash[:success] = l(:label_plugin_git_reg_success)
-      redirect_to :action => 'index'
+      redirect_to :action => 'index', :flash => flash
     else
       render 'new'
     end
@@ -43,31 +43,46 @@ class GitMainController < ApplicationController
 
     if @git_user.update_attributes(params[:git_user])
       flash[:success] = l(:label_plugin_git_account_update_successful)
-      redirect_to :action => 'index'
+      redirect_to :action => 'index', :flash => flash
     else
       flash[:error] = l(:label_plugin_git_account_update_error)
-      render 'index'
+      redirect_to :action => 'index', :flash => flash
     end
   end
 
   def upload_key
     @git_user = GitUser.find(User.current.id)
     uploaded_key = params[:ssh_public]
-    if uploaded_key.size < Setting.plugin_chiliproject_git['max_file_ssh_size']
-      path = "#{Setting.plugin_chiliproject_git['dir_ssh_public']}/#{@git_user.login}/"
-      File.open("#{path}#{uploaded_key.original_filename}", 'w') do |file|
-        file.write(uploaded_key.read)
-      end
-
-      new_key = GitUsersKey.new
-      new_key.user_id = User.current.id
-      new_key.file_name = uploaded_key.original_filename
-      new_key.save
-
-      flash[:success] = l(:label_plugin_git_public_key_upload)
+    if uploaded_key.nil?
+      flash[:error] = l(:label_plugin_git_you_must_select_file)
     else
-      flash[:error] = l(:label_plugin_git_big_size_public)
+      original_filename = uploaded_key.original_filename
+      if uploaded_key.size < Setting.plugin_chiliproject_git['max_file_ssh_size']
+        file_name = "#{Setting.plugin_chiliproject_git['dir_ssh_public']}/#{@git_user.login}/#{uploaded_key.original_filename}"
+        if File.exist? file_name
+          i = 0
+          while File.exists? "#{file_name}#{i}"
+            i+=1
+          end
+          file_name += i.to_s
+          original_filename += i.to_s
+        end
+
+        File.open("#{file_name}", 'w') do |file|
+          file.write(uploaded_key.read)
+        end
+
+        new_key = GitUsersKey.new
+        new_key.user_id = User.current.id
+        new_key.file_name = original_filename
+        new_key.save
+
+        flash[:success] = l(:label_plugin_git_public_key_upload)
+        @user_keys = GitUsersKey.all(:conditions => {:user_id => User.current.id})
+      else
+        flash[:error] = l(:label_plugin_git_big_size_public)
+      end
     end
-    render 'index'
+    redirect_to :action => 'index', :flash => flash
   end
 end
