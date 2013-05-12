44a4eaa38116708d082cc221beff712481e5ac64
Denis Karyakin <samhane.me@gmail.com>
2013-05-12 23:48:44 +0400

    In admin view add user.lastname and other

--- app/models/git_user.rb
+++ app/models/git_user.rb
@@ -1,6 +1,7 @@
 class GitUser < ActiveRecord::Base
   unloadable
   belongs_to :user
+  has_one :user, :foreign_key => 'id'
   validates_length_of :login, :in => 2..100, :message => l(:label_plugin_git_login_length_error)
   validates_uniqueness_of :login, :id, :message => l(:label_plugin_git_login_already_isset)
 
